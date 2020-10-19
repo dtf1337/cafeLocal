e099c2babde8fd5dabfb34b405b654fe76c1d50d
Bislam S <bislam1337@gmail.com>
2020-10-19 15:36:39 -0600

    googlemaps 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
  // Initialize and add the map
  function initMap() {
-    // The location of Uluru
+    // The location of LOCATION
     const location = { lat: 39.739235, lng: -104.990250 };
-    // The map, centered at Uluru
+    // The map, centered at LOCATION
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: location,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at LOCATION
     const marker = new google.maps.Marker({
       position: location,
       map: map,
